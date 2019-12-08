7a637c94d473e460598f02586b187e011a799279
85 <542574439@qq.com>
2019-12-08 20:49:54 +0800

    热部署

--- src/main/java/com/example/demo/controller/HelloController.java
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -1,16 +1,23 @@
 package com.example.demo.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-@RequestMapping("hello")
+@Controller
 public class HelloController {
 
     @GetMapping("/{name}")
     public String hello(@PathVariable String name){
         return "hello:" +name;
     }
+
+    @RequestMapping("/login")
+    public String userLogin() {
+        return "login";
+    }
+
+
 }
